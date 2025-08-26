912128f2a11f7a00b563c5a6324c441709fca7ed
Zilou692 <louis.caron62128@gmail.com>
2025-08-26 13:09:19 +0200

    Update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Louis7Twitch" },
+            { "message": "Salut je suis du code.", "author": "Louis7Twitch" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Louis7Twitch" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
